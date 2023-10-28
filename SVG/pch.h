59b99854388a8bead5e9ce0ecc515d3570bc0691
calvadosss <124347873+Nhannguyenus24@users.noreply.github.com>
2023-10-29 00:17:37 +0700

    Add files via upload

---
+++ SVG/pch.h
@@ -0,0 +1,29 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+#define WIN32_LEAN_AND_MEAN   // Exclude rarely-used stuff from Windows headers
+// add headers that you want to pre-compile here
+#include "framework.h"
+#include "targetver.h"
+// Windows Header Files:
+#include <windows.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+
+
+
+
+#endif //PCH_H
