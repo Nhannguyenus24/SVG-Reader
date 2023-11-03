05a589582de7e1756fa6dd96b9ae07cf661fc1d1
Nhannguyenus24 <ntnhan223@clc.fitus.edu.vn>
2023-11-03 22:32:20 +0700

    add zoom in/out button

--- SVG-Reader/SVG-Reader/Drawing.h
+++ SVG-Reader/SVG-Reader/Drawing.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef DRAWING_H
+#define DRAWING_H
 #include "framework.h"
 #include <windows.h>
 #include <objidl.h>
@@ -22,3 +23,5 @@ VOID DrawPolygon(HDC hdc, vector<int> xP, vector<int> yP, float f_alpha, int f_r
 VOID DrawPolyline(HDC hdc, vector<int> xP, vector<int> yP, float f_alpha, int f_red, int f_green, int f_blue, float s_alpha, int s_red, int s_green, int s_blue, int strokeWidth);
 
 VOID DrawText(HDC hdc, int x, int y, string text, float f_alpha, int f_red, int f_green, int f_blue, int strokeWidth);
+
+#endif //DRAWING_H
